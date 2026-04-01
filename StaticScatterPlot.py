b3bf5886cdb3c4120f4512acd9b7da9aecbbec94
shreyasoff1cial25 <shreyasroy750@gmail.com>
2026-04-01 10:03:04 +0530

    Create StaticScatterPlot.py

---
+++ StaticScatterPlot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 13, 17, 20]
+
+plt.scatter(x, y)
+
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Scatter Plot with Grid")
+
+plt.grid(True)
+
+plt.show()
